11dac055885eb4e6459136c0ea530bd753f02017
Simonas Sankauskas <simonas.sank@gmail.com>
2023-08-04 18:10:03 +0300

    Change app package to dev.simonas.quies (#1)

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-const val PUBLISHING_GROUP = "com.ncorti.kotlin.template"
+const val PUBLISHING_GROUP = "dev.simonas"
 
 object AppCoordinates {
-    const val APP_ID = "com.ncorti.kotlin.template.app"
-    const val APP_VERSION_NAME = "1.0.0"
+    const val APP_ID = "dev.simonas.quies"
+    const val APP_VERSION_NAME = "0.0.0"
     const val APP_VERSION_CODE = 1
 }
