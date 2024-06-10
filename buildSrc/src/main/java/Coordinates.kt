adff2ed7bdf753b8b39d8f0e5d182a60f5ed815b
Simonas Sankauskas <simonas.sank@gmail.com>
2024-06-10 23:39:16 +0300

    Version Bump

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -2,6 +2,6 @@ const val PUBLISHING_GROUP = "dev.simonas"
 
 object AppCoordinates {
     const val APP_ID = "dev.simonas.quies"
-    const val APP_VERSION_NAME = "0.12.1"
-    const val APP_VERSION_CODE = 27
+    const val APP_VERSION_NAME = "0.12.2"
+    const val APP_VERSION_CODE = 28
 }
