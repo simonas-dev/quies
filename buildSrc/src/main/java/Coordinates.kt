f8c8933126fcddb5214db47c1f56aa3ccbb9a7a6
Simonas Sankauskas <simonas.sank@gmail.com>
2025-09-11 00:24:50 +0300

    App Release

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -2,6 +2,6 @@ const val PUBLISHING_GROUP = "dev.simonas"
 
 object AppCoordinates {
     const val APP_ID = "dev.simonas.quies"
-    const val APP_VERSION_NAME = "1.0.0"
-    const val APP_VERSION_CODE = 32
+    const val APP_VERSION_NAME = "1.0.1"
+    const val APP_VERSION_CODE = 33
 }
